778eedf9c2402224a3e4f5a3021916414b6e2811
Dzmitry Ivanou <dimiv007@gmail.com>
2024-07-13 02:54:01 +0200

    feat: allowed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.views import description, photo, stash_campaign, submission
 
+origins = [
+    '*'
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(description.router, prefix="/api", tags=["description"])
 app.include_router(photo.router, prefix="/api", tags=["photo"])
